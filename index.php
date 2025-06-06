08f35ed7adcf72292b0e08e54e60e7dda775e34c
HugoRollierEPSI <hugo.rollier@ecoles-epsi.net>
2025-06-06 11:10:57 +0200

    finale

--- index.php
+++ index.php
@@ -29,15 +29,6 @@ include 'db.php';
     <div class="container">
         <div class="box available">
             <h2>Matériel Disponible</h2>
-=======
-        <h1>Location de MatÃ©riel Informatique</h1>
-        <a href="login.php" class="btn">Connexion</a>
-    </header>
-
-    <div class="container">
-        <div class="box available">
-            <h2>Disponible</h2>
->>>>>>> 203f44517fa04d9c16fad29264ff86fcffc2f28e
             <?php
             $stmt = $pdo->query("SELECT libelle FROM materielle WHERE estDisponible = 1");
             foreach ($stmt as $row) {
@@ -45,14 +36,9 @@ include 'db.php';
             }
             ?>
         </div>
-<<<<<<< HEAD
 
         <div class="box unavailable">
             <h2>Matériel Indisponible</h2>
-=======
-        <div class="box unavailable">
-            <h2>Indisponible</h2>
->>>>>>> 203f44517fa04d9c16fad29264ff86fcffc2f28e
             <?php
             $stmt = $pdo->query("SELECT libelle FROM materielle WHERE estDisponible = 0");
             foreach ($stmt as $row) {
